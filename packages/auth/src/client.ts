4a9420eff845216658beca05dec3f47f7ee41c7b
faroukarar <farouk.arar69@gmail.com>
2026-02-13 19:18:44 +0300

    refactor(auth): modularize auth factory and schema for better extensibility

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,9 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_APP_URL?.replace(/\/+$/, "") ||
-  "http://localhost:3000";
-
-export const authClient = createAuthClient({
-  baseURL,
-});
+export const authClient = (baseURL: string) =>
+  createAuthClient({
+    baseURL,
+  });
--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -1,29 +1,22 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
-import type { PgDatabase, PgQueryResultHKT } from "drizzle-orm/pg-core";
-import {
-  type ExtractTablesWithRelations,
-  type TablesRelationalConfig,
-} from "drizzle-orm";
+import type { BetterAuthOptions } from "better-auth";
+import type { PgDatabase } from "drizzle-orm/pg-core";
 
-export function createAuth<
-  TQueryResult extends PgQueryResultHKT,
-  TFullSchema extends Record<string, unknown>,
-  TSchema extends TablesRelationalConfig =
-    ExtractTablesWithRelations<TFullSchema>,
->(
-  db: PgDatabase<TQueryResult, TFullSchema, TSchema>,
-  socialProviders?: { google?: { clientId: string; clientSecret: string } },
-  options?: any,
+export function createAuth(
+  db: PgDatabase<any, any, any>,
+  authSchema: Record<string, any>,
+  options?: Partial<BetterAuthOptions>,
 ) {
   return betterAuth({
     database: drizzleAdapter(db, {
       provider: "pg",
+      schema: authSchema,
     }),
-
-    socialProviders,
+    plugins: [nextCookies(), ...(options?.plugins || [])],
     ...options,
-    plugins: [nextCookies()],
   });
 }
+
+export type Auth = ReturnType<typeof betterAuth>;
